905e9d079ca802f4d3284e3defd599acc6adc725
Alef Gomes <alefgomesandre@gmail.com>
2023-03-14 20:57:36 -0300

    add route to update a user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,6 @@ const UsersController = require("../controller/UsersController");
 const usersController = new UsersController();
 
 usersRoutes.post("/users", usersController.create);
+usersRoutes.put("/users", usersController.update);
 
 module.exports = usersRoutes;
