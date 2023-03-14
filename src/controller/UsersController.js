c221792c4fcf7df099d30536de0d4eaa16c002b6
Alef Gomes <alefgomesandre@gmail.com>
2023-03-14 17:56:02 -0300

    Creating a function to create a new user

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,5 +1,22 @@
+const knex = require("../database/knex");
+const { encryptPassword } = require("../utils/encryptPassword");
+const { checkEmailExists } = require("../utils/checkEmailExists");
+const { checkFieldIsEmpty } = require("../utils/checkFieldIsEmpty");
+
 class UsersController {
-  
+  async create(req, res) {
+    const { name, email, password } = req.body;
+
+    checkFieldIsEmpty(req.body); // verify if any field is empty
+
+    await checkEmailExists(email); // verify if email already exists
+
+    const passwordEncrypted = await encryptPassword(password); // encrypt password
+
+    await knex("users").insert({ name, email, password: passwordEncrypted }); // insert user in database
+
+    return res.status(201).json({ message: "User created successfully" });
+  }
 }
 
 module.exports = UsersController;
