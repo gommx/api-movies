22c3ca9ec2d7aaec5bc279e63475ec6ddc674322
Alef Gomes <alefgomesandre@gmail.com>
2023-03-16 11:20:39 -0300

    add method to create movie_notes

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -1,6 +1,50 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { checkFieldIsEmpty } = require("../utils/checkFieldIsEmpty");
+
 class NotesController {
   async create(req, res) {
-    res.status(201).json({ message: "movie created successfully" });
+    // create a new note
+    const { title, description, rating, tags } = req.body; // get data from request body
+    const { user_id } = req.headers; // get user_id from request headers
+
+    // verify if any field is empty
+    checkFieldIsEmpty(req.body);
+    checkFieldIsEmpty(tags);
+    checkFieldIsEmpty(user_id);
+
+    const user = await knex("users").where({ id: user_id }).first(); // get user from database
+
+    // verify if user exists
+    if (!user) {
+      throw new AppError("user not found");
+    }
+
+    // verify if rating is valid
+    const isRatingValid = rating >= 0 && rating <= 5;
+
+    if (!isRatingValid) {
+      throw new AppError("invalid rating");
+    }
+
+    // verify if movie already exists
+    const isMovieRepeated = await knex("movie_notes").where({ user_id, title }).first();
+
+    if (isMovieRepeated) {
+      throw new AppError("have you registered this movie before");
+    }
+
+    // create a new note
+    const newNote = await knex("movie_notes")
+      .insert({ title, description, rating, user_id })
+      .returning("id"); // return the id of the new note
+
+    const note_id = newNote[0].id; // get the id of the new note
+
+    // create tags
+    await knex("movie_tags").insert({ note_id, user_id, name: tags });
+
+    res.status(201).json({ message: "movie note created successfully" });
   }
 }
 
